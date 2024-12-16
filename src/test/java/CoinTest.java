dd3caa83c72431a3ea9811fdedd1405b89e7813b
Tiago Zou Yin <up202306438@edu.fe.up.pt>
2024-12-16 21:04:49 +0000

    CoinTest

---
+++ src/test/java/CoinTest.java
@@ -0,0 +1,43 @@
+import com.t03g06.model.elements.Coin;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class CoinTest {
+    private Coin coin;
+
+    @BeforeEach
+    void setUp() {
+        coin = new Coin(100, 50);
+    }
+
+    @Test
+    void testMoveLeft() {
+        coin.moveLeft(10);
+        assertEquals(90, coin.getX());
+    }
+
+    @Test
+    void testMoveYMovingDown() {
+        final int maxY = 200;
+        coin.moveY(maxY);
+        assertEquals(51, coin.getY());
+    }
+
+    @Test
+    void testMoveYDirectionChange() {
+    }
+
+
+    @Test
+    void testIsOutOfScreen() {
+        final int coinWidth = 2;
+
+        assertFalse(coin.isOutOfScreen());
+
+        coin.moveLeft(coin.getX() + coinWidth + 1);
+        assertTrue(coin.isOutOfScreen());
+    }
+
+}
