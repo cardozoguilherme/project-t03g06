7f7bac55a646750ac0feb8954afa525b097c5063
Tiago Zou Yin <up202306438@edu.fe.up.pt>
2024-12-18 14:40:30 +0000

    BirdviewerTest

---
+++ src/test/java/view/elements/BirdViewerTest.java
@@ -0,0 +1,38 @@
+package view.elements;
+
+import com.googlecode.lanterna.graphics.TextGraphics;
+import com.googlecode.lanterna.TextColor;
+import com.t03g06.model.elements.Bird;
+import com.t03g06.view.elements.BirdViewer;
+import com.t03g06.model.GameConstants;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.*;
+
+import static org.mockito.Mockito.*;
+
+class BirdViewerTest {
+
+    @Mock
+    private TextGraphics tg;
+
+    private Bird bird;
+    private BirdViewer birdViewer;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+
+        bird = new Bird(10);
+        birdViewer = new BirdViewer(bird, tg);
+    }
+
+    @Test
+    void testDraw() {
+        birdViewer.draw();
+
+        verify(tg).setForegroundColor(TextColor.ANSI.YELLOW_BRIGHT);
+
+        verify(tg).putString(GameConstants.WIDTH / 4, bird.getY(), "O");
+    }
+}
